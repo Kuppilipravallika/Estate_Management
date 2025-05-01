c1691c3d37a1e17483345034030052bfdc57fc88
Kuppilipravallika <kuppilipravallika.22.csm@anits.edu.in>
2025-05-01 22:53:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
-import os  
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
 # MongoDB config
-app.config["MONGO_URI"] =os.getenv("MONGO_URI") 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/swift_estates"
 
 mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
